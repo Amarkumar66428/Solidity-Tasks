f1f3d37233c349dda7dc060faea1f9b3a64dc7af
Amar kumar <amar66428@gmail.com>
2023-03-17 13:34:20 +0530

    initial commit

--- identity.sol
+++ identity.sol
@@ -3,18 +3,18 @@ pragma solidity >= 0.5.0 < 0.9.0;
 
 contract identity
 {
-    string name;
+    string _name;
     uint age;
 
     constructor() public
     {
-        name = "Monu";
+        _name = "Monu";
         age = 17;
     }
 
     function getName() view public returns(string memory)
     {
-        return name;
+        return _name;
     }
     function getAge() view public returns(uint)
     {
@@ -24,4 +24,21 @@ contract identity
      {
          age += 1;
      }
+
+
+function check(string memory str) public pure returns(string memory)
+    {
+      bytes memory a = bytes(str);
+      uint len = a.length;
+
+      for(uint i=0;i< len / 2 ;i++)
+      {
+
+          if(a[i] != a[len -1 -i])
+          {
+              return "Not Palindrome";
+          }
+      }
+      return "Palindrome";
+    }
 }
